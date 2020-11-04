9b632a52b1788449448c95e8d9d127fe351f127d
helder-hanka <hfernandes238@gmail.com>
2020-11-04 16:18:46 +0100

    add import component Gallery

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import NavMenu from './components/Home/NavMenu';
+import Gallery from './components/Gallery/Gallery';
 import './App.css';
 
 function App () {
   return (
     <div className="App">
       <NavMenu />
+      <Gallery />
     </div>
   );
 }
