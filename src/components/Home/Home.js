26908d4db8c1a9d84ca7ef258fde92ceaa13a0b0
yann-rzd <yann.rouzaud@gmail.com>
2020-11-05 19:05:51 +0100

    Adding logo

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -13,7 +13,17 @@ class Home extends React.Component {
       "url('./Assets/painting-3135875_1920.jpg')",
       "url('./Assets/pexels-ann-h-1765033.jpg')",
       "url('./Assets/pexels-anni-roenkae-2457284.jpg')",
-      "url('./Assets/pexels-madison-inouye-1831234.jpg')"
+      "url('./Assets/pexels-madison-inouye-1831234.jpg')",
+      "url('./Assets/pexels-moose-photos-1037999.jpg')",
+      "url('./Assets/pexels-paula-schmidt-963486.jpg')",
+      "url('./Assets/pexels-pixabay-163811.jpg')",
+      "url('./Assets/pexels-prateek-katyal-2694434.jpg')",
+      "url('./Assets/pexels-santiago-pagnotta-1702624.jpg')",
+      "url('./Assets/pexels-stas-knop-2916450.jpg')",
+      "url('./Assets/pexels-steve-johnson-1145720.jpg')",
+      "url('./Assets/pexels-steve-johnson-1484759.jpg')",
+      "url('./Assets/pexels-toa-heftiba-şinca-1194420.jpg')",
+      "url('./Assets/pexels-zaksheuskaya-1568607.jpg')"
     ];
     const random = Math.floor(Math.random() * backgrounds.length) + 0;
 
@@ -35,6 +45,9 @@ class Home extends React.Component {
         className="home"
         style={ this.state.background }
       >
+        <div className="logo">
+          <img src="./Assets/logo_V4.svg" alt="Wild Art Logo"/>
+        </div>
         <div className="menu">
           <NavMenu />
         </div>
