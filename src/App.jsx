c190c238a8e49ec20e1afe83bc3999a8e15eaf7c
Fredy <fredy.ly.wcs@gmail.com>
2020-11-10 11:03:17 +0100

    cleaning navmenu branch

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import NavMenu from './components/NavMenu';
-// import NavMenuLine from './components/NavMenuLine';
 import './App.css';
 
 function App () {
   return (
     <div className="App">
       <NavMenu />
-      {/* <NavMenuLine /> */}
     </div>
   );
 }
