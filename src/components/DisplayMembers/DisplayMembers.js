1c0bbbe6ed17ae72b6c031d53ed8b2f0900d6590
helder-hanka <hfernandes238@gmail.com>
2020-11-12 14:35:56 +0100

    add onMouseOver and onMouseOut

--- src/components/DisplayMembers/DisplayMembers.js
+++ src/components/DisplayMembers/DisplayMembers.js
@@ -1,21 +1,25 @@
-import React, { useState } from 'react';
+import React, { useRef } from 'react';
 import { PropTypes } from 'prop-types';
 import './DisplayMembers.css';
 const DisplayMembers = ({ avatar, name }) => {
-  const [isShown, setIsShown] = useState(false);
-  console.log(isShown);
+  // const [isShown, setIsShown] = useState(false);
+  // console.log(isShown);
+  const hoverImg = useRef(null);
+  console.log(hoverImg);
 
   const addHoverImg = () => {
-    console.log('Hover');
-    setIsShown(true);
+    hoverImg.current.classList.add('addHover');
+  };
+
+  const clearImg = () => {
+    if (hoverImg.current.classList.contains('addHover')) {
+      hoverImg.current.classList.remove('addHover');
+    }
   };
 
   return (
-        <div className="diplayMembers">
-          <div
-            onMouseOver={addHoverImg}
-            className=""
-          >
+        <div ref={hoverImg} className="diplayMembers">
+          <div onMouseOver={addHoverImg} onMouseOut={clearImg} >
             <img src={avatar} alt={name} />
           </div>
         </div>
