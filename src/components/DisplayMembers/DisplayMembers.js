538aa60acfa89b6e52cef361ca6213222c08a451
helder-hanka <hfernandes238@gmail.com>
2020-11-12 15:42:17 +0100

    add div content class and updated diplayMembersContainer class

--- src/components/DisplayMembers/DisplayMembers.js
+++ src/components/DisplayMembers/DisplayMembers.js
@@ -1,28 +1,38 @@
-import React, { useRef } from 'react';
+import React from 'react';
 import { PropTypes } from 'prop-types';
 import './DisplayMembers.css';
 const DisplayMembers = ({ avatar, name }) => {
   // const [isShown, setIsShown] = useState(false);
   // console.log(isShown);
-  const hoverImg = useRef(null);
-  console.log(hoverImg);
+  // const hoverImg = useRef(null);
+  // console.log(hoverImg);
 
-  const addHoverImg = () => {
-    hoverImg.current.classList.add('addHover');
-  };
+  // const addHoverImg = () => {
+  //   hoverImg.current.classNameList.add('addHover');
+  // };
 
-  const clearImg = () => {
-    if (hoverImg.current.classList.contains('addHover')) {
-      hoverImg.current.classList.remove('addHover');
-    }
-  };
+  // const clearImg = () => {
+  //   if (hoverImg.current.classNameList.contains('addHover')) {
+  //     hoverImg.current.classNameList.remove('addHover');
+  //   }
+  // };
 
+  // <div ref={hoverImg} classNameName="diplayMembers">
+  //   <div onMouseOver={addHoverImg} onMouseOut={clearImg} >
+  //     <img src={avatar} alt={name} />
+  //   </div>
+  // </div>
   return (
-        <div ref={hoverImg} className="diplayMembers">
-          <div onMouseOver={addHoverImg} onMouseOut={clearImg} >
+      <div className="diplayMembersContainer">
+        <div className="content">
+            <div className="content-overlay"></div>
             <img src={avatar} alt={name} />
-          </div>
+            <div className="content-details fadeIn-top fadeIn-right">
+              <h3>This is a title</h3>
+              <p>This is a short description</p>
+            </div>
         </div>
+      </div>
   );
 };
 
