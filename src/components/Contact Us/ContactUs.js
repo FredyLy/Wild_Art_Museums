98be8011d6a527b7a2bc4b11989c466bd82b21e9
Grégory <grg.vilar@gmail.com>
2020-11-12 19:31:01 +0100

    Contact Us component OK but not responsive for the moment.

--- src/components/Contact Us/ContactUs.js
+++ src/components/Contact Us/ContactUs.js
@@ -8,6 +8,7 @@ import logoMap from './assets/map.png';
 import logoEmail from './assets/email.png';
 import logoPhone from './assets/telephone.png';
 
+const MAX_LENGTH = 150;
 class ContactUs extends Component {
   constructor (props) {
     super(props);
@@ -16,13 +17,26 @@ class ContactUs extends Component {
       LastName: '',
       Mail: '',
       Phone: '',
-      Message: ''
+      Message: '',
+      character: ''
     };
+
     this.handleChangeFirstName = this.handleChangeFirstName.bind(this);
     this.handleChangeLastName = this.handleChangeLastName.bind(this);
     this.handleChangeMail = this.handleChangeMail.bind(this);
     this.handleChangePhone = this.handleChangePhone.bind(this);
     this.handleChangeMessage = this.handleChangeMessage.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleSubmit (event) {
+    event.preventDefault();
+  }
+
+  handleChange (event) {
+    if (event.target.value.length <= MAX_LENGTH) {
+      this.setState({ character: event.target.value });
+    }
   }
 
   handleChangeFirstName (event) {
@@ -42,51 +56,66 @@ class ContactUs extends Component {
   }
 
   handleChangeMessage (event) {
-    this.setState({ Message: event.target.value });
+    this.setState({ character: event.target.value });
   }
 
   render () {
+    const maximumReached = this.state.character.length >= MAX_LENGTH;
+    const numRemaining = MAX_LENGTH - this.state.character.length;
+
     return (
             <div className="fullpage1">
             <div>
                 <div className="ContactUs">
                     <div className="contact-information">
-                    <div className="text-justify">
-                <h1>Contact Information</h1>
-                <p>Fill up the formand our Team will get back to you within 24 hours</p>
-                <p><img className="logo" src={logoMap} />44 rue Alphonse Penaud, 75020 Paris</p>
-                <p><img className="logo" src={logoEmail} />paris@wildcodeschool.com</p>
-                <p><img className="logo" src={logoPhone} />+33 (0)7 84 39 08 74</p>
-                <div>
-                        <img className="network-logo" src={logoTwitter} alt="logo twitter" />
-                        <img className="network-logo" src={logoFacebook} alt="logo Facebook" />
-                        <img className="network-logo" src={logoLinkedin} alt="logo Linkedin" />
-                        <img className="network-logo" src={logoInstagram} alt="logo Instagram" />
+                      <ul className="text-justify">
+                        <li className="title-justify list-justify">Contact Information</li>
+                        <li className="first-text-justify">Fill up the formand our Team will get back to you within 24 hours.</li>
+                <li className="list-justify"><img className="logo " src={logoMap} />44 rue Alphonse Penaud,<br />75020 Paris</li>
+                <li className="list-justify"><img className="logo" src={logoEmail} />paris@wildcodeschool.com</li>
+                <li className="list-justify"><img className="logo" src={logoPhone} />+33 (0)7 84 39 08 74</li>
+                      </ul>
+                <div className="logo-bar">
+                <a href="https://twitter.com/WildCodeSchool?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor" target="_blank" rel="noreferrer" alt="Twitter Wild Code School">
+                  <img className="network-logo" src={logoTwitter} alt="logo twitter" />
+                  </a>
+                <a href="https://fr-fr.facebook.com/WildCodeSchool/" rel="noreferrer" target="_blank" alt="Facebook Wild Code School">
+                  <img className="network-logo" src={logoFacebook} alt="logo Facebook" />
+                  </a>
+                <a href="https://www.linkedin.com/school/wild-code-school/" rel="noreferrer" target="_blank" alt="Linkedin Wild Code School">
+                  <img className="network-logo" src={logoLinkedin} alt="logo Linkedin" />
+                </a>
+                <a href="https://www.instagram.com/wildcodeschoolofficial/?hl=fr" rel="noreferrer" target="_blank" alt="Instagram Wild Code School">
+                  <img className="network-logo" src={logoInstagram} alt="logo Instagram" />
+                </a>
                     </div>
-                </div>
                     </div>
                     <div className="formulaire-content">
                     <form className="formulaire">
                         <label className="row1" htmlFor="FirstName">First Name</label>
                         <label className="row1" htmlFor="LastName" >Last Name</label>
-                        <input id="First Name" type="text" value={this.state.FirstName} onChange={this.handleChangeFirstName} />
-                        <input id="Last Name" type="text" className="element-4" value={this.state.LastName} onChange={this.handleChangeLastName} />
+                        <input autoComplete="off" id="First Name" type="text" value={this.state.FirstName} onChange={this.handleChangeFirstName} />
+                        <input autoComplete="off" id="Last Name" type="text" className="element-4" value={this.state.LastName} onChange={this.handleChangeLastName} />
                         <label htmlFor="Mail" className="element-5">Mail</label>
                         <label htmlFor="Phone" className="element-6">Phone : </label>
-                        <input id="Mail" type="text" className="element-7" value={this.state.Mail} onChange={this.handleChangeMail} />
-                        <input id="Phone" type="text" className="element-8" value={this.state.Phone} onChange={this.handleChangePhone} />
+                        <input autoComplete="off" id="Mail" type="text" className="element-7" value={this.state.Mail} onChange={this.handleChangeMail} />
+                        <input autoComplete="off" id="Phone" type="text" className="element-8" value={this.state.Phone} onChange={this.handleChangePhone} />
                         <label htmlFor="Message" className="element-9">Message : </label>
-                        <input id="Message" type="text" className="element-10" value={this.state.Message} onChange={this.handleChangeMessage} />
-                            <button type="button" value="SendMessage" className="element-11" >Send Message</button>
+                        <textarea
+                        className={maximumReached ? 'length-maximum-reached' : 'length-ok'}
+                        id="Message"
+                        type="text"
+                        value={this.state.character}
+                        onChange={this.handleChange}
+                        />
+                        <small className="element-11">{numRemaining} remaining characters</small>
+
+                            <button type="submit" value="SendMessage" className="element-12" >Send Message</button>
                     </form>
                     </div>
             </div>
             </div>
-            <div className="fullpage2"></div>
-            <div className="fullpage3">
             <div className="Location">
-            </div>
-
             </div>
             </div>
     );
