1973d80701dc1b9e8ffecb9793e3267780aa3b6d
Christopher Saury <saurychristopher@gmail.com>
2020-11-13 08:49:10 +0100

    add the props to the DisplayMembers file

--- src/components/DisplayMembers/DisplayMembers.js
+++ src/components/DisplayMembers/DisplayMembers.js
@@ -1,21 +1,18 @@
-import React, { useState } from 'react';
+import React, { useRef } from 'react';
 import { PropTypes } from 'prop-types';
 import './DisplayMembers.css';
-const DisplayMembers = ({ avatar, name }) => {
-  const [isShown, setIsShown] = useState(false);
-  console.log(isShown);
 
-  const addHoverImg = () => {
-    console.log('Hover');
-    setIsShown(true);
-  };
+const DisplayMembers = ({ avatar, name }) => {
+  const hoverImg = useRef(null);
+  console.log(hoverImg);
+  // const addHoverImg = () => {
+  // console.log('Hover');
+  //   setIsShown(true);
+  // };
 
   return (
-        <div className="diplayMembers">
-          <div
-            onMouseOver={addHoverImg}
-            className=""
-          >
+        <div className='diplayMembers'>
+          <div ref={hoverImg} >
             <img src={avatar} alt={name} />
           </div>
         </div>
