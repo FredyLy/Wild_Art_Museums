1186c7cfc420721659e9c7e6086090b5f59abc0e
yann-rzd <yann.rouzaud@gmail.com>
2020-11-18 14:16:23 +0100

    first commit

--- src/index.jsx
+++ src/index.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom';
+import ThemeContext from './contexts/ThemeContext';
 import './index.css';
 import App from './App';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>,
+  <ThemeContext>
+    <React.StrictMode>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </React.StrictMode>
+  </ThemeContext>,
   document.getElementById('root')
 );
