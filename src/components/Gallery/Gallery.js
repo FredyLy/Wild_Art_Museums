e684e5295dda0ea06d12e058742bc8e47a54d664
Delphine <delphine.pva@gmail.com>
2020-11-25 12:36:02 +0100

    Gallry CSS nice

--- src/components/Gallery/Gallery.js
+++ src/components/Gallery/Gallery.js
@@ -1,8 +1,8 @@
 import React, { Component, Fragment } from 'react';
 import Axios from 'axios';
-import Loader from '../Loader/Loader';
 import Location from '../Location/Location';
 import Modal from '../Modal/Modal';
+import Loading from '../Home/Loading';
 
 import './Gallery.css';
 
@@ -64,16 +64,16 @@ class Gallery extends Component {
   render () {
     const { data } = this.state;
     const decision = (data
-      ? <div className="gallery"> {data.map((data) =>
+      ? <div className="container-gallery"> {data.map((data) =>
       <Fragment key={data.title}>
         {data.images.map((image) =>
-          <div onClick={() => this.showModal(data)} className="gallery-divImg" key={image.idsid}>
+          <div onClick={() => this.showModal(data)} className="gallery-img" key={image.idsid}>
             <img className="galleryImg" src={image.baseimageurl} alt={data.title}/>
           </div>)
         }
       </Fragment>)}
       </div>
-      : <Loader loadingMsg={'Loading...'} styling={{ textAlign: 'center', marginTop: '20%', color: 'red' }}/>);
+      : <Loading />);
 
     const displayBtnNext = !this.state.url.next;
     const displayBtnPrev = !this.state.url.prev;
@@ -87,39 +87,48 @@ class Gallery extends Component {
             <button onClick={this.closeModal}>X</button>
           </div>
           <div className="modalBody">
-          <div className="comicImage">
-            <a href={characterInfos.primaryimageurl} target='_blank' rel="noopener noreferrer" ><img src={characterInfos.primaryimageurl} alt={characterInfos.title} /></a>
-          </div>
+            <div className="comicImage">
+              <a href={characterInfos.primaryimageurl} target='_blank' rel="noopener noreferrer" ><img src={characterInfos.primaryimageurl} alt={characterInfos.title} /></a>
+            </div>
           <div className="comicDetails">
           <h3>Identification and Creation</h3>
-            <p>People : </p>
-            <p>Title : {characterInfos.title}</p>
-            <p>Classification : {characterInfos.classification}</p>
-            <p>Work Type : </p>
-            <p>Date : {characterInfos.dated}</p>
-            <p>Period : {characterInfos.period}</p>
-            <p>Culture : {characterInfos.culture}</p>
+            <p>People : {characterInfos.people && characterInfos.people.map((people, i) => {
+              return (
+                <span key={i}>{people.name}</span>
+              );
+            })}</p>
+            {characterInfos.title && <p>Title : {characterInfos.title}</p>}
+            {characterInfos.classification && <p>Classification : {characterInfos.classification}</p>}
+            <p>Work Type : {characterInfos.worktypes && characterInfos.worktypes.map((worktype, i) => {
+              return (
+                <span key={i}>{worktype.worktype}</span>
+              );
+            })}</p>
+            {characterInfos.dated && <p>Date : {characterInfos.dated}</p>}
+            {characterInfos.period && <p>Period : {characterInfos.period}</p>}
+            {characterInfos.culture && <p>Culture : {characterInfos.culture}</p>}
 
           <h3>Physical Descriptions</h3>
-            <p>Medium : {characterInfos.medium}</p>
-            <p>Dimension : {characterInfos.dimensions}</p>
+            {characterInfos.medium && <p>Medium : {characterInfos.medium}</p>}
+            {characterInfos.technique && <p>Technique : {characterInfos.technique}</p>}
+            {characterInfos.dimensions && <p>Dimension : {characterInfos.dimensions}</p>}
 
           <h3>Provenance </h3>
-            <p>{characterInfos.provenance}</p>
+            {characterInfos.provenance && <p>{characterInfos.provenance}</p>}
 
           <h3>Acquisition and rights</h3>
-            <p>Credit Line : Harvard Art Museums/Arthur M. Sackler Museum, Ernest B. and Helen Pratt Dane Fund for the Acquisition of Oriental Art </p>
-            <p>Accession Year : {characterInfos.accessionyear}</p>
-            <p>Object Number : {characterInfos.objectnumber}</p>
-            <p>Division : {characterInfos.division}</p>
-            <p>Contact : {characterInfos.contact}</p>
+            {characterInfos.creditline && <p>Credit Line : {characterInfos.creditline}</p>}
+            {characterInfos.accessionyear && <p>Accession Year : {characterInfos.accessionyear}</p>}
+            {characterInfos.objectnumber && <p>Object Number : {characterInfos.objectnumber}</p>}
+            {characterInfos.division && <p>Division : {characterInfos.division}</p>}
+            {characterInfos.contact && <p>Contact : {characterInfos.contact}</p>}
 
             <p>The Harvard Art Museums encourage the use of images found on this website for personal, noncommercial use, including educational and scholarly purposes.
             To request a higher resolution file of this image, please submit an online request.</p>
 
             <p>This record was created from historic documentation and may not have been reviewed by a curator; it may be inaccurate or incomplete. Our records are frequently revised and enhanced.
               For more information please contact the {characterInfos.division} at am_asianmediterranean@harvard.edu</p>
-            <a className="btn-info" href={characterInfos.url} target='_blank' rel="noopener noreferrer" >More info</a>
+            <a href={characterInfos.url} target='_blank' rel="noopener noreferrer">More info</a>
           </div>
         </div>
       </Fragment>)
@@ -129,7 +138,7 @@ class Gallery extends Component {
             <h2>Chargement</h2>
           </div>
           <div className="modalBody">
-            <Loader loadingMsg={'Loading...'} styling={{ textAlign: 'center', marginTop: '20%', color: 'red' }}/>
+            <Loading />
           </div>
       </Fragment>);
 
