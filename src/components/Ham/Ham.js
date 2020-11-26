aa9f69a795cd5773e78f25b7cff1c6dff1b4d53c
Fredy <fredy.ly.wcs@gmail.com>
2020-11-26 15:11:55 +0100

    correcting component call on HAM

--- src/components/Ham/Ham.js
+++ src/components/Ham/Ham.js
@@ -5,8 +5,8 @@ import Busch from './Busch';
 import Sackler from './Sackler';
 import Intro from './Intro';
 import MuseumSelector from './MuseumSelector';
-import LocationMobile from '../LocationMobile/LocationMobile';
-// import Location from '../Location/Location';
+// import LocationMobile from '../LocationMobile/LocationMobile';
+import Location from '../Location/Location';
 
 import './Ham.css';
 import SocialMedia from './SocialMedia';
@@ -56,10 +56,10 @@ class Ham extends Component {
         </div>
       </div>
       <div className="ham-location">
-      {/* <Location title={this.state.title} /> */}
+      <Location title={this.state.title} />
       </div>
       <div className="ham-locationMobile">
-      <LocationMobile />
+      {/* <LocationMobile /> */}
       </div>
       </div>
     );
