115cd0ae36ce5ef4006aa887f94eb87cb54a592f
Fredy <fredy.ly.wcs@gmail.com>
2020-12-08 23:50:56 +0100

    changing modal description text css

--- src/components/Gallery/Gallery.js
+++ src/components/Gallery/Gallery.js
@@ -84,57 +84,63 @@ class Gallery extends Component {
       <Fragment>
         <div className="modalHeader">
             <button onClick={this.closeModal}>X</button>
+        </div>
+
+        <div className="modalBody">
+          <div className="comicImage">
+              <a href={characterInfos.primaryimageurl} target='_blank' rel="noopener noreferrer" >
+                <img src={characterInfos.primaryimageurl} alt={characterInfos.title} />
+              </a>
           </div>
-          <div className="modalBody">
-            <div className="comicImage">
-              <a href={characterInfos.primaryimageurl} target='_blank' rel="noopener noreferrer" ><img src={characterInfos.primaryimageurl} alt={characterInfos.title} /></a>
-            </div>
+
           <div className="comicDetails">
-          <h3>Identification and Creation</h3>
-            {characterInfos.people &&
-              <p>People : {characterInfos.people.map((people, i) => {
-                return (
-                  <span key={i}>{people.name}</span>
-                );
-              })}
-              </p>
-            }
-            {characterInfos.title && <p>Title : {characterInfos.title}</p>}
-            {characterInfos.classification && <p>Classification : {characterInfos.classification}</p>}
+              <h3>Identification and Creation</h3>
+                {characterInfos.people &&
+                  <p>
+                    <div className="modal-span">People</div> {characterInfos.people.map((people, i) => {
+                      return (
+                        <span key={i}>{people.name}</span>
+                      );
+                    })}
+                  </p>
+                }
+                {characterInfos.title && <p><div className="modal-span">Title</div>{characterInfos.title}</p>}
+                {characterInfos.classification && <p><div className="modal-span">Classification</div> {characterInfos.classification}</p>}
 
-            {characterInfos.worktypes &&
-              <p>Work Type : {characterInfos.worktypes.map((worktype, i) => {
-                return (
-                  <span key={i}>{worktype.worktype}</span>
-                );
-              })}
-              </p>
-            }
-            {characterInfos.dated && <p>Date : {characterInfos.dated}</p>}
-            {characterInfos.period && <p>Period : {characterInfos.period}</p>}
-            {characterInfos.culture && <p>Culture : {characterInfos.culture}</p>}
+                {characterInfos.worktypes &&
+                  <p><div className="modal-span">Work Type</div> {characterInfos.worktypes.map((worktype, i) => {
+                    return (
+                      <span key={i}>{worktype.worktype}</span>
+                    );
+                  })}
+                  </p>
+                }
+                {characterInfos.dated && <p><div className="modal-span">Date</div> {characterInfos.dated}</p>}
+                {characterInfos.period && <p><div className="modal-span">Period</div> {characterInfos.period}</p>}
+                {characterInfos.culture && <p><div className="modal-span">Culture</div> {characterInfos.culture}</p>}
 
-          <h3>Physical Descriptions</h3>
-            {characterInfos.medium && <p>Medium : {characterInfos.medium}</p>}
-            {characterInfos.technique && <p>Technique : {characterInfos.technique}</p>}
-            {characterInfos.dimensions && <p>Dimension : {characterInfos.dimensions}</p>}
+              <h3>Physical Descriptions</h3>
+                {characterInfos.medium && <p><div className="modal-span">Medium</div> {characterInfos.medium}</p>}
+                {characterInfos.technique && <p><div className="modal-span">Technique</div> {characterInfos.technique}</p>}
+                {characterInfos.dimensions && <p><div className="modal-span">Dimension</div> {characterInfos.dimensions}</p>}
 
-          <h3>Provenance </h3>
-            {characterInfos.provenance && <p>{characterInfos.provenance}</p>}
+              <h3>Provenance </h3>
+                {characterInfos.provenance && <p>{characterInfos.provenance}</p>}
 
-          <h3>Acquisition and rights</h3>
-            {characterInfos.creditline && <p>Credit Line : {characterInfos.creditline}</p>}
-            {characterInfos.accessionyear && <p>Accession Year : {characterInfos.accessionyear}</p>}
-            {characterInfos.objectnumber && <p>Object Number : {characterInfos.objectnumber}</p>}
-            {characterInfos.division && <p>Division : {characterInfos.division}</p>}
-            {characterInfos.contact && <p>Contact : {characterInfos.contact}</p>}
+              <h3>Acquisition and rights</h3>
+                {characterInfos.creditline && <p><div className="modal-span">Credit Line</div> {characterInfos.creditline}</p>}
+                {characterInfos.accessionyear && <p><div className="modal-span">Accession Year</div> {characterInfos.accessionyear}</p>}
+                {characterInfos.objectnumber && <p><div className="modal-span">Object Number</div> {characterInfos.objectnumber}</p>}
+                {characterInfos.division && <p><div className="modal-span">Division</div> {characterInfos.division}</p>}
+                {characterInfos.contact && <p><div className="modal-span">Contact</div> {characterInfos.contact}</p>}
 
-            <p>The Harvard Art Museums encourage the use of images found on this website for personal, noncommercial use, including educational and scholarly purposes.
-            To request a higher resolution file of this image, please submit an online request.</p>
+                <p className="modal-copy">The Harvard Art Museums encourage the use of images found on this website for personal, noncommercial use, including educational and scholarly purposes.
+                To request a higher resolution file of this image, please submit an online request.
+                <br/>
+                This record was created from historic documentation and may not have been reviewed by a curator; it may be inaccurate or incomplete. Our records are frequently revised and enhanced.
+                For more information please contact the {characterInfos.division} at am_asianmediterranean@harvard.edu</p>
 
-            <p>This record was created from historic documentation and may not have been reviewed by a curator; it may be inaccurate or incomplete. Our records are frequently revised and enhanced.
-              For more information please contact the {characterInfos.division} at am_asianmediterranean@harvard.edu</p>
-            <a href={characterInfos.url} target='_blank' rel="noopener noreferrer">More Info</a>
+                <a href={characterInfos.url} target='_blank' rel="noopener noreferrer">More Info</a>
           </div>
         </div>
       </Fragment>)
