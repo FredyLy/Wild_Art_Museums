64e20446d9c2b220210d2e1599c7400ec07ff53a
Fredy <fredy.ly.wcs@gmail.com>
2020-12-24 13:01:29 +0100

    fixing closeModal

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,17 +1,18 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
 
 import './Modal.css';
 
-const Modal = ({ showModal, children }) => {
-  return (
+const Modal = ({ showModal, children, closeModal }) => {
+  return ReactDOM.createPortal(
     showModal && (
-      <div className="modalBackground">
+      <div className="modalBackground" onClick={closeModal}>
         <div className="modalContainer">
           {children}
         </div>
       </div>
-    )
+    ), document.getElementById('second-root')
   );
 };
 
